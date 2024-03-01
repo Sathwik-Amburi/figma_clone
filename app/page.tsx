d007bf26366cf3de50bf48fd49445c01b310f06d
Sathwik Amburi <sathwik.amburi@tum.de>
2024-03-02 00:27:57 +0100

    init

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Room } from "./Room";
+
+export default function Page() {
+  return (
+    <>
+      <div className="h-[100vh] w-full flex justify-center items-center text-center">
+        <h1 className="text-2xl text-white"> Live Figma Clone</h1>
+      </div>
+    </>
+  );
+}
